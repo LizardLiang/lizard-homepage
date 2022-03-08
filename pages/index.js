3797d1adcb24c0e8805a9e703a6c6486076e8a83
LizardLiang <shotup0101@gmail.com>
2022-03-08 23:25:39 +0800

    add paragraph for About me

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import { Heading, Container, Box } from '@chakra-ui/react'
+import {
+  Heading,
+  Container,
+  Box,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
 
 const Page = () => {
   return (
     <Container>
       <Box
         borderRadius="lg"
-        bg="red"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
         p={3}
         mb={6}
         align="center"
@@ -29,13 +37,24 @@ const Page = () => {
             borderWidth={2}
             borderStyle="solid"
             maxWidth="100px"
+            maxHeight="100px"
+            boxSize="100"
+            fit="cover"
             display="inline-block"
             borderRadius="full"
             src="/images/lizard.png"
-            alt="Profile"
+            alt="Profile Image"
           />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          {`I'm a enthusiastic engineer from Taiwan, love to learn everything about coding.`}
+        </Paragraph>
+      </Section>
     </Container>
   )
 }
