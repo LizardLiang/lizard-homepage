5ed6bd74a553c544bd440ffba7d6932abee60ef7
Lizard <shotup0101@gmail.com>
2022-03-09 18:57:19 +0800

    finished

---
+++ pages/works.js
@@ -0,0 +1,31 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbnailMyWork1 from '../public/images/works/code.png'
+
+const Works = () => {
+  return (
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="myWork1"
+              title="MyWork1"
+              thumbnail={thumbnailMyWork1}
+            >
+              My work 1 description
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Works
+
---
+++ pages/works/myWork1.js
@@ -0,0 +1,29 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => {
+  return (
+    <Layout title="Inkdrop">
+      <Container>
+        <Title>
+          MyWork1 <Badge>2016</Badge>
+        </Title>
+        <P>My work 1 description goes here</P>
+        <List ml={4} my={4}>
+          <ListItem>
+            <Meta>Stack</Meta>
+            <span>{`C++ / Qt Creator`}</span>
+          </ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+        </List>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Work
